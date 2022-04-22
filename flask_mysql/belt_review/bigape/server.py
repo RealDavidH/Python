1257bf7af33bfda967c4c03491a41736afec8730
David Hollinger <100631847+RealDavidH@users.noreply.github.com>
2022-04-22 09:43:47 -0700

    Delete flask_mysql/belt_review directory

--- flask_mysql/belt_review/bigape/server.py
+++
@@ -1,5 +0,0 @@
-from flask import redirect
-from flask_app import app
-from flask_app.controllers import sighting_controller, user_controller, routes, skeptic_controller
-if __name__ == "__main__":
-    app.run(debug=True)
--- flask_mysql/belt_review/recipes/flask_app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from flask_bcrypt import Bcrypt
-app = Flask(__name__)
-bcrypt = Bcrypt(app)
-DATABASE = "recipesdb"
-app.secret_key = 'kekekekeke'
